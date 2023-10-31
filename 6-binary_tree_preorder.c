6417cbd794f73970d39bd0521090d3b361a5bea1
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-10-31 00:31:09 -0700

    Errors in code fixed

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,4 +1,4 @@
-#inlude "binary_trees.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_preorder - goes through a binary tree
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	l_height = binary_tree_height(tree->left);
-	r_height = height_tree_height(tree->right);
+	r_height = binary_tree_height(tree->right);
 
 	return ((l_height > r_height ? l_height : r_height) + 1);
 }
