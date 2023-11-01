2f0c3f5b8ea670a43ed29c34f1d2fea653b62d93
1CyBeR-J1 <marvellousojeleke@gmail.com>
2023-10-31 20:57:02 -0700

    doc error and styling corrected

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -51,10 +51,10 @@ int _pow_recursion(int x, int y)
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
 
 
@@ -72,11 +72,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-//	l_height = binary_tree_height(tree->left);
-//	r_height = binary_tree_height(tree->right);
-//
-//	return ((l_height > r_height ? l_height : r_height) + 1);
 
 	l_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	r_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
